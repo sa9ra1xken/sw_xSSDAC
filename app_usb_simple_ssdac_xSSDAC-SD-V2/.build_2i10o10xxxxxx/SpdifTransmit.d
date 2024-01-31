bd37c6399ab1cdb453bb7c2059c0d99d187f934c
sa9ra1xken <150714506+sa9ra1xken@users.noreply.github.com>
2024-01-31 22:26:45 +0900

    Delete app_usb_simple_ssdac_xSSDAC-SD-V2/.build_2i10o10xxxxxx directory

--- app_usb_simple_ssdac_xSSDAC-SD-V2/.build_2i10o10xxxxxx/SpdifTransmit.d
+++
@@ -1,3 +0,0 @@
-SpdifTransmit.o: \
-  C:/Users/takaaki/git/sw_xSSDAC/module_spdif_tx/src/SpdifTransmit.xc
-
--- app_usb_simple_ssdac_xSSDAC-SD-V2/.build_2i10o10xxxxxx/_m_usb_audio/usb_buffer/xc_ptr.xc.d
+++
@@ -1,5 +0,0 @@
-.build_2i10o10xxxxxx/_m_usb_audio/usb_buffer/xc_ptr.xc.d .build_2i10o10xxxxxx/_m_usb_audio/usb_buffer/xc_ptr.xc.o .build_2i10o10xxxxxx/_m_usb_audio/usb_buffer/xc_ptr.xc.pca.xml: \
-  C:/Users/takaaki/git/sw_xSSDAC/module_usb_audio/usb_buffer/xc_ptr.xc \
-  C:/Users/takaaki/git/sw_xSSDAC/module_usb_audio/usb_buffer/xc_ptr.h
-
-
